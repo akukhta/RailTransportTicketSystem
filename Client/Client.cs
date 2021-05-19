32418fc561520cd853bf3b378ea0be655f0bbd01
Alexey Kuhta <electricsix23@gmail.com>
2021-05-19 22:55:23 +0300

    ~~~

---
+++ Client/Client.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class ClientConnection
+    {
+        private Socket ClientSocket;
+        private IPAddress Ip;
+        private IPEndPoint ServerEndPoint;
+        
+        public enum UserType
+        {
+            DefaultUser,
+            Admin,
+            Error
+        }
+
+        public enum Operations : byte
+        {
+            Login
+        }
+
+        public ClientConnection(string IpString = "127.0.0.1", int Port = 5564)
+        {
+            Ip = IPAddress.Parse(IpString);
+            ServerEndPoint = new IPEndPoint(Ip, Port);
+            ClientSocket = new Socket(Ip.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            ClientSocket.Bind(ServerEndPoint);
+        }
+
+        private void SendToClient(byte[] Buffer)
+        {
+            ClientSocket.Send(Buffer);
+        }
+
+        private byte[] ReceiveForClient()
+        {
+            byte[] Buffer = new byte[ClientSocket.Available];
+            ClientSocket.Receive(Buffer);
+            return Buffer;
+        }
+
+        public UserType Login(string ID)
+        {
+            List<byte> buffer = new List<byte>();
+            
+            buffer.Add((byte)Operations.Login);
+            buffer.AddRange(BitConverter.GetBytes(ID.Length));
+            buffer.AddRange(Encoding.UTF8.GetBytes(ID));
+            
+            SendToClient(buffer.ToArray());
+            
+            byte[] answer = ReceiveForClient();
+
+            switch (answer[0])
+            {
+                case 0:
+                    return UserType.DefaultUser;
+;
+                case 1:
+                    return UserType.Admin;
+
+                case 2:
+                    return UserType.Error;
+
+                default:
+                    return UserType.Error;
+            }
+        
+        }
+    }
+}
--- Server/FunctionsHandler.cs
+++ Server/FunctionsHandler.cs
@@ -8,9 +8,55 @@ namespace Server
 {
     class FunctionsHandler
     {
-        public static byte[] HandleRequest(byte[] request)
+        public List<byte> HandleRequest(List<byte> request)
         {
-            return new byte[1];
+            byte func = request[0];
+            request.RemoveAt(0);
+            List<byte> answer;
+
+            switch (func)
+            {
+                case 0:
+                    answer = Sign(request);
+                    break;
+
+                case 1:
+                    answer = Sotr(request);
+                    break;
+
+                case 2:
+                    answer = Predpr(request);
+                    break;
+
+                case 3:
+                    answer = SotrPredpr(request);
+                    break;
+
+                case 4:
+                    break
+
+            }
         }
+
+        private List<byte> Sign(List<byte> buffer)
+        {
+            return new List<byte>();
+        }
+
+        private List<byte> Sotr(List<byte> buffer)
+        {
+            return new List<byte>();
+        }
+
+        private List<byte> Predpr(List<byte> buffer)
+        {
+            return new List<byte>();
+        }
+
+        private List<byte> SotrPredpr(List<byte> buffer)
+        {
+            return new List<byte>();
+        }
+
     }
 }
